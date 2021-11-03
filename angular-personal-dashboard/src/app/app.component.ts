f9fe9af792fda47fdf9d5e60b7352f5b8f4f125b
Mapacherama <jerome_tesselaar@hotmail.com>
2021-11-03 10:38:12 +0100

    Part 6: Route Transitions is done
    Next up: https://youtu.be/zcJnlGW46hs .

--- angular-personal-dashboard/src/app/app.component.ts
+++ angular-personal-dashboard/src/app/app.component.ts
@@ -8,7 +8,7 @@ import { RouterOutlet } from '@angular/router';
   styleUrls: ['./app.component.scss'],
   animations: [
     trigger('routeAnim', [
-      transition('* => *', [
+      transition(':increment', [
         style({
           position: 'relative',
           overflow: 'hidden'
@@ -33,29 +33,70 @@ import { RouterOutlet } from '@angular/router';
             query(':leave',[
               animate('200ms ease-in', style({
                 opacity: 0,
-                transform: 'translateX(-80px)'
+                transform: 'translateX(-50px)'
               }))
           ], { optional: true }),
           query(':enter', [
             style({
-              transform: 'translateX(80px)',
+              transform: 'translateX(50px)',
               opacity: 0
             }),
-            animate(250, style({
+            animate('250ms 100ms ease-out', style({
               opacity: 1,
               transform: 'translateX(0)'
             }))
           ], { optional: true })
           ])
-      ])
+      ]),
 
-    ])
+      transition(':decrement', [
+        style({
+          position: 'relative',
+          overflow: 'hidden'
+        }),
+
+        query(':enter, :leave', [
+          style({
+            // display: 'block',
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%'
+          })
+        ], { optional: true }),
+
+        group([
+          query(':leave', [
+            animate('200ms ease-in', style({
+              opacity: 0,
+              transform: 'translateX(50px)'
+            }))
+          ], { optional: true }),
+
+          query(':enter', [
+            style({
+              transform: 'translateX(-50px)',
+              opacity: 0
+            }),
+            animate('250ms 100ms ease-out' , style({
+              opacity: 1,
+              transform: 'translateX(0)'
+            }))
+          ], { optional: true })
+        ])
+      ])
+    ]),
   ]
 })
 
 export class AppComponent {
   prepareRoute(outlet: RouterOutlet){
-    if(outlet.isActivated) return outlet.activatedRoute.snapshot.url;
+    if(outlet.isActivated) {
+      const tab = outlet.activatedRouteData['tab'];
+      if(!tab) return 'secondary'
+      return tab;
+    }
   }
 
 
